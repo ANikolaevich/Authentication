8e0dbcdaf630833918cb1cae602dccff0f7bfd93
Artem <artem.khodakivskyi@gmail.com>
2026-02-02 17:30:12 +0200

    Study project

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,31 @@
-
+private static HashMap<String, User> userList = new HashMap<>();
 Scanner scanner = new Scanner(System.in);
 
 void main() {
+
     createNewUser();
-    loginAndPasswordRequest();
-
-}
-
-private void loginAndPasswordRequest() {
-    while(true) {
+    String enteredUserName;
+    while (true) {
         System.out.println("Enter user name");
-        String enteredUserName = scanner.nextLine().toLowerCase();
-        System.out.println("Enter password");
-        String userPass = scanner.nextLine();
-        if(Authentication.loginVerification(enteredUserName, userPass)){
-            break;
-        };
+        enteredUserName = scanner.nextLine().toLowerCase();
+        if(authentication(enteredUserName)) break;
     }
 }
 
+private boolean authentication(String enteredUserName) {
+    String userPass;
+    System.out.println("Enter password");
+    userPass = scanner.nextLine();
+    if (Authentication.loginVerification(enteredUserName, userPass)) {
+        return true;
+    }
+    return false;
+}
 
 private static void createNewUser() {
     User user1 = new User();
     user1.setUserList("pavlenko1996", user1);
-    user1.setUserEMail("oleksandr.pavlenko@mail.com");
+    user1.setUserEMail("oleksandr.Pavlenko@mail.com");
     user1.setUserName("pavlenko1996");
     user1.setPassword("1q2W3e$r");
 }
